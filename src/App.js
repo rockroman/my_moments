d82b4c6aafa4831b243c2a2b310df0466533cb4d
Roman Rakic <rock.rakic@gmail.com>
2023-11-02 22:12:25 +0000

    feat: add 3 basic routes with react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
-import './App.css';
-
+import NavBar from "./components/NavBar";
+import styles from "./App.module.css";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <h1>My moments</h1>
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign in Page</h1>} />
+          <Route exact path="/signup" render={() => <h1>Sign Up Page</h1>} />
+          <Route render={() => <h1>Page not found!</h1>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
